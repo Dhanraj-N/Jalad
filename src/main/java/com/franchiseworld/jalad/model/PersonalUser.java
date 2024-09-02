25766f6bd4c2400758711a50b1afccb9dc1628e5
Sunil Vitthal Rankhamb <sunilrankhamb19081@gmail.com>
2024-09-02 21:48:46 +0530

    Revert "ResponseHandler package commited"
    This reverts commit c6ca228f16d9089a725fa1bae1bfde9d4ef66cde.

--- src/main/java/com/franchiseworld/jalad/model/PersonalUser.java
+++ src/main/java/com/franchiseworld/jalad/model/PersonalUser.java
@@ -1,15 +1,9 @@
 package com.franchiseworld.jalad.model;
 
-import java.util.Set;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
 import lombok.Data;
 
 @Entity
@@ -18,27 +12,17 @@ public class PersonalUser {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+	private Long personalUserId;
 
 	private String firstName;
 	private String lastName;
-	private String email;
+	private String emailId;
 	private String address;
 	private String country;
 	private String state;
 	private String city;
 	private String pincode;
-	private long contactNo;
+	private Long contactNo;
 	private String password;
-	
-	
-	// Many-to-many relationship with PersonalCourier
-    @ManyToMany(cascade = CascadeType.ALL)
-    @JoinTable(
-        name = "personal_user_courier", // Name of the join table
-        joinColumns = @JoinColumn(name = "personal_user_id"), // Foreign key for PersonalUser
-        inverseJoinColumns = @JoinColumn(name = "personal_courier_id") // Foreign key for PersonalCourier
-    )
-    private Set<PersonalCourier> couriers;
 
 }
